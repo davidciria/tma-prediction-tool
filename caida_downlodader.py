0e466e995d625cc84fc16473ade672667cea3ebf
David Ciria <david.ciria01@alumni.upf.edu>
2022-10-12 18:38:57 +0200

    Divide datasets in dirA and dirB

--- caida_downlodader.py
+++ caida_downlodader.py
@@ -5,7 +5,6 @@ import os
 session = requests.session()
 
 base_dir = "./pcap_stats/" # Dir where will be downloaded all the data.
-os.makedirs(base_dir, exist_ok=True)
 base_url = "https://publicdata.caida.org/datasets/passive/passive-oc48/20020814-160000.UTC/pcap/"
 
 response = session.get(base_url)
@@ -14,8 +13,10 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 for a in soup.find_all('a'):
     if a.get('href') and "pcap.stats" == a.get('href')[-10::]:
+        dir = a.get('href').split('.')[1]
         download_url = base_url + a.get('href')
         print("Downloading file from: {}".format(download_url))
         response = session.get(download_url)
-        with open(base_dir + a.get('href'), 'wb') as downloaded_file:
+        os.makedirs(base_dir + '/{}/'.format(dir), exist_ok=True)
+        with open(base_dir + '/{}/'.format(dir) + a.get('href'), 'wb') as downloaded_file:
             downloaded_file.write(response.content)
--- time_series_generator.py
+++ time_series_generator.py
@@ -7,71 +7,75 @@ from IPython.display import display
 
 base_dir = "./pcap_stats/"
 
-file_names = [f for f in listdir(base_dir)]
+dir_names = [f for f in listdir(base_dir)]
 
-timeseries = []
-timestamps = []
+for dir in dir_names:
+    print(dir)
+    file_names = [f for f in listdir(base_dir + dir)]
+    timeseries = []
+    timestamps = []
 
-def get_ipv4_bytes(line):
-    if "IPv4 bytes:" == line[0:11]:
-        key, value_raw = line.split(":")
-        value = int(value_raw.strip())
-        return key, value
-    
-    return None
+    def get_ipv4_bytes(line):
+        if "IPv4 bytes:" == line[0:11]:
+            key, value_raw = line.split(":")
+            value = int(value_raw.strip())
+            return key, value
+        
+        return None
 
-def get_timestamp(line, type):
-    if type + " timestamp:" == line[0:len(type) + 11]:
-        key, value_raw = line.split(":")
-        value = float(value_raw.strip())
-        return key, value
-    
-    return None
+    def get_timestamp(line, type):
+        if type + " timestamp:" == line[0:len(type) + 11]:
+            key, value_raw = line.split(":")
+            value = float(value_raw.strip())
+            return key, value
+        
+        return None
 
-#################
-# Data cleaning #
-#################
+    #################
+    # Data cleaning #
+    #################
 
-for i, fn in enumerate(file_names):
-    pcap_stats_file = open(base_dir + fn, "r")
+    for i, fn in enumerate(file_names):
+        pcap_stats_file = open(base_dir + '{}/'.format(dir) + fn, "r")
 
-    for line in pcap_stats_file.readlines():
-        if get_ipv4_bytes(line): ipv4_bytes_key, ipv4_bytes_value = get_ipv4_bytes(line)
-        if get_timestamp(line, "First"): first_timestamp_key, first_timestamp_value = get_timestamp(line, "First")
-        if get_timestamp(line, "Last"): last_timestamp_key, last_timestamp_value = get_timestamp(line, "Last")
+        for line in pcap_stats_file.readlines():
+            if get_ipv4_bytes(line): ipv4_bytes_key, ipv4_bytes_value = get_ipv4_bytes(line)
+            if get_timestamp(line, "First"): first_timestamp_key, first_timestamp_value = get_timestamp(line, "First")
+            if get_timestamp(line, "Last"): last_timestamp_key, last_timestamp_value = get_timestamp(line, "Last")
 
-    if ipv4_bytes_key and ipv4_bytes_value: timeseries.append(ipv4_bytes_value)
-    if (first_timestamp_key and first_timestamp_value) and (last_timestamp_key and last_timestamp_value): timestamps.append([first_timestamp_value, last_timestamp_value])
+        if ipv4_bytes_key and ipv4_bytes_value: timeseries.append(ipv4_bytes_value)
+        if (first_timestamp_key and first_timestamp_value) and (last_timestamp_key and last_timestamp_value): timestamps.append([first_timestamp_value, last_timestamp_value])
 
-    pcap_stats_file.close()
+        pcap_stats_file.close()
 
-########################
-# Dataframe generation #
-########################
+    ########################
+    # Dataframe generation #
+    ########################
 
-init_datetimes = [datetime.utcfromtimestamp(t[0]) for t in timestamps]
-end_datetimes = [datetime.utcfromtimestamp(t[1]) for t in timestamps]
+    init_datetimes = [datetime.utcfromtimestamp(t[0]) for t in timestamps]
+    end_datetimes = [datetime.utcfromtimestamp(t[1]) for t in timestamps]
 
-dataframe_values = list(zip(init_datetimes, end_datetimes, timeseries))
-dataframe = pandas.DataFrame(dataframe_values, columns=['init_date', 'end_date', 'bytes'])
+    dataframe_values = list(zip(init_datetimes, end_datetimes, timeseries))
+    dataframe = pandas.DataFrame(dataframe_values, columns=['init_date', 'end_date', 'bytes'])
 
-display(dataframe)
+    display(dataframe)
 
-# Save dataframe to a CSV file.
-dataframe.to_csv('./time_series_df.csv', index = False) 
+    # Save dataframe to a CSV file.
+    dataframe.to_csv('./time_series_df_{}.csv'.format(dir), index = False) 
 
-###################
-# Timeseries plot #
-###################
+    ###################
+    # Timeseries plot #
+    ###################
 
-x_values = np.array([(i + 5) for i in range(60)])
-y_values = np.array([t/(1024 * 1024) for t in timeseries]) # Convert Bytes to MB.
+    x_values = np.array([(i + 5) for i in range(len(timestamps))])
+    y_values = np.array([t/(1024 * 1024) for t in timeseries]) # Convert Bytes to MB.
 
-fig, ax = plt.subplots()
-plt.plot(x_values, y_values)
-ax.ticklabel_format(useOffset=False)
-ax.ticklabel_format(style='plain')
-plt.xlabel("Step")
-plt.ylabel("MB")
-plt.title("Step vs Total Bytes")
-plt.show()
+    fig, ax = plt.subplots()
+    plt.plot(x_values, y_values)
+    ax.ticklabel_format(useOffset=False)
+    ax.ticklabel_format(style='plain')
+    plt.xlabel("Step")
+    plt.ylabel("MB")
+    plt.ylim(0, np.max(y_values) + np.max(y_values)/2)
+    plt.title("Step vs Total Bytes")
+    plt.show()
